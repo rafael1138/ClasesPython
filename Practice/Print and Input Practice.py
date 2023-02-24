ef7f609cca88b33e32bc9439d008856412eedbde
Rafa <rafael1138@protonmail.com>
2023-02-23 23:00:17 -0500

    Update agregando practica de input, print y variables

---
+++ Practice/Print and Input Practice.py
@@ -0,0 +1,9 @@
+#practice with input and print.
+
+#print('Tu Nombre y Apellido son ' + input('Tu Nombre: ') + ' ' + input('Tu Apellido: '))
+
+#Avanzado con Input y Variable
+
+name = input('Tu Nombre: ')
+last = input('Tu Apellido: ')
+print('Tu Nombre es: ' + name + ' ' + last)
