c2e2ed5ebc38fc9e94b953bf43b70eead432deaf
Rafael <rafael1138@protonmail.com>
2023-02-23 22:11:30 -0500

    master

---
+++ Dia 2/Day2.py
@@ -0,0 +1,28 @@
+#Dia 2
+
+# Tipos de Datos
+
+'''
+String (str) Es un texto, siempre esta en comillas. --> "hola", "1138"
+
+Integer (int) Es un numero entero, nunca esta en comillas. --> 1138, 2023
+
+Floating (float) Es un numero con decimales, nunca estan en comillas. --> 20.5, 20.0, 11.38
+
+Lists (list) Es una coleccion ordenada de objetos, estan entre [] y pueden contener str,int,float,etc
+--> ["juan",1,3.5,"hola"] Su posicion se denomina Indice y comineza en 0. "hola" es indice 4.
+Sus elementos pueden ser repetidos.
+
+Dictionary (dic) Su contenido se escribe entre {} Y se comprende de pares de palabras agrupdas.
+En un (dic) es una CLAVE y su VALOR --> {"color":"rojo","arte":"cine"} No tienen un orden/indice dentro del diccionario.
+
+Tuples (tuple) Son parecidos a las listas pero su orden es inmutable. --> ('lun','mar','mie','jue','vie')
+Sus elementos pueden ser repetidos.
+
+Sets (set) Es un conjunto ordenado de elementos UNICOS. --> {'a','b','c'}
+
+Booleanos (bool) --> TRUE, FALSE (Verdadero o Falso)
+
+'''
+
+
---
+++ Dia 2/Dia 2 - Variables.py
@@ -0,0 +1,10 @@
+#VARIABLES
+# Basicamente puedes pensar que una variable es una caja, que contiene algo que puede cambiar.
+# VARIABLE = ALOQUETUDIGAS
+
+nombre = "Juan"
+print(nombre)
+
+nombre="Laura"
+print(nombre)
+
