02d15a9ae5fa889a6e6c7220b140cbc0b9a83fb0
Rafael <rafael1138@protonmail.com>
2023-02-25 01:12:10 -0500

    Pruebas y clases de variables desde win

--- Dia 2/Day2 - Variables.py
+++ Dia 2/Day2 - Variables.py
@@ -8,3 +8,24 @@ print(nombre)
 nombre="Laura"
 print(nombre)
 
+edad1 = 10
+edad2 = 20
+print(edad1+edad2) #se puede usar una variable para calcular
+
+nombre = input('Dime Tu Nombre: ')
+apellido = input('tu apellido por favor: ')
+print('Tu nombre es: ' + nombre)
+Completo = nombre + apellido
+print(Completo)
+
+curso = 'Python'
+frase = 'Estas haciendo un curso de ' + curso
+print(frase)
+
+#tratar de escribir variables cuyo nombre sea logico
+#la variable debe ser una palabra, no espacios si usas mas de una palabra.
+#se suele usar minusculas para las variables y no usar acentos ni ñ.
+#se pueden usar numeros pero no al inicio de la variable
+#variables no deben tener simbolos
+#tampoco se pueden usar palabras claves/reservadas como print o input
+
