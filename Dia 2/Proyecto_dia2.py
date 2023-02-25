216c5e0a1ec617cf1ac1d30102285966c4e6bdf6
Rafael <rafael1138@protonmail.com>
2023-02-25 14:20:06 -0500

    Pruebas y clases de variables desde win

---
+++ Dia 2/Proyecto_dia2.py
@@ -0,0 +1,11 @@
+#Proyecto de dia 2
+#De hacerse un programa que le pregunte al vendedor su nombre, monto de ventas, y le debe decir
+#la comision ganada calculada al 13%
+
+nombre = input('Dime tu Nombre: ')
+ventas = input('Cuanto has vendido hoy?: ')
+porcentaje = 0.13
+total = int(ventas) * 13/100
+
+
+print(f'{nombre} tu comision es {total} que viene a ser el 13% de {ventas}')
